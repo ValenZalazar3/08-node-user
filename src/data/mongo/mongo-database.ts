6d90e07b537175175b9782d1fbb1997b2d2a35c2
Valen <valenzalazar3@gmail.com>
2024-05-11 19:40:04 -0300

    Curso node seccion 18

---
+++ src/data/mongo/mongo-database.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+
+
+
+interface Options {
+    mongoUrl: string;
+    dbName: string;
+}
+
+export class MongoDatabase {
+
+    static async connect(options: Options) {
+        const { mongoUrl, dbName } = options;
+
+        try {
+            await mongoose.connect(mongoUrl, {
+                dbName
+            })
+            // console.log('mongo connected') por si no anda la conección aca se prueba y hace.
+            return true
+        } catch (error) {
+            console.log('Mongo connection error');
+            throw error;
+        }
+    }
+}
+
