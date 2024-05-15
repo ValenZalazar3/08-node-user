51c7a86e9144792a3e814a72a838d04b9742d6e6
Valen <valenzalazar3@gmail.com>
2024-05-14 22:23:06 -0300

    Add(register, login and validateEmail)

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -1,5 +1,7 @@
 // Los DTOs se utilizan comúnmente en arquitecturas de software que siguen el principio de separación de preocupaciones, donde diferentes capas de la aplicación tienen responsabilidades específicas y están separadas entre sí. Por ejemplo, en una arquitectura de tres capas típica (interfaz de usuario, lógica de negocio y capa de acceso a datos), los DTOs pueden utilizarse para transferir datos entre estas capas de manera eficiente y sin acoplarlas demasiado.
 
+import { regularExps } from "../../../config";
+
 
 
 
@@ -12,9 +14,16 @@ export class RegisterUserDto {
     ) { }
 
 
-    static create(object: { [key: string]: any }): [string?, RegisterUserDto] {
+    static create(object: { [key: string]: any }): [string?, RegisterUserDto?] {
+        const { name, email, password } = object;
 
+        if (!name) return ['Missing name'];
+        if (!email) return ['Missing email'];
+        if (!regularExps.email.test(email)) return ['Email is not valid'];
+        if (!password) return ['Missing password'];
+        if (password.length < 6) return ['Password is too short'];
 
+        return [undefined, new RegisterUserDto(name, email, password)]
     }
 }
 
