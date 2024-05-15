c548ac7c1f09bf724e05956b133c6c4ef9984030
Valen <valenzalazar3@gmail.com>
2024-05-14 22:23:10 -0300

    Add(register, login and validateEmail)

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { AuthController } from './controller';
+import { AuthService, EmailService } from '../services';
+import { envs } from '../../config';
+
 
 
 
@@ -10,8 +13,14 @@ export class AuthRoutes {
     static get routes(): Router {
 
         const router = Router();
+        const emailService = new EmailService(
+            envs.MAILER_SERVICE,
+            envs.MAILER_EMAIL,
+            envs.MAILER_SECRET_KEY,
+        )
+        const authService = new AuthService(emailService);
 
-        const controller = new AuthController()
+        const controller = new AuthController(authService)
 
         // Definir las rutas
         router.post('/login', controller.loginUser);
